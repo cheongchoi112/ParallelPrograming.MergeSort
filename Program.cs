1be475e5bee1d67187231813f902443d5077abfb
Joe Choi <cheongchoi112@gmail.com>
2023-12-19 23:21:15 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParallelPrograming.FinalProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
